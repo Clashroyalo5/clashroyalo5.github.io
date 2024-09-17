4f13748b617d40b52af473068f19b33480412e0e
Clashyolalo5 <121621804+Clashroyalo5@users.noreply.github.com>
2024-09-16 19:34:04 -0500

    Update script.js

--- colegio/lengua/tecnicas-grupales-expo/script.js
+++ colegio/lengua/tecnicas-grupales-expo/script.js
@@ -1,7 +1,7 @@
 document.getElementById('downloadBtn').addEventListener('click', function() {
     const link = document.createElement('a');
     link.href = 'https://clashroyalo5.github.io/colegio/lengua/tecnicas-grupales-expo/Técnicas%20Grupales.pptx'; // Asegúrate de que la ruta sea correcta
-    link.download = 'diapositivas tecnicas grupales.pdf'; // Nombre del archivo que se descargará
+    link.download = 'diapositivas tecnicas grupales.pptx'; // Nombre del archivo que se descargará
     link.click();
   });
   
@@ -13,7 +13,7 @@ document.getElementById('downloadBtn').addEventListener('click', function() {
   
   document.getElementById('whatsappBtn').addEventListener('click', function() {
     // Cambia la URL de "tu-pagina.com" a la de tu página web.
-    const whatsappUrl = `https://api.whatsapp.com/send?text=Mira%20Esta%20Es%20La%20Página%20De%20La%20Tecnica%20Grupal:%20Mesa%20Redonda.`;
+    const whatsappUrl = `https://api.whatsapp.com/send?text=Mira%20Esta%20Es%20La%20Página%20De%20La%20Tecnica%20Grupal:%20Mesa%20Redonda:%20https://clashroyalo5.github.io/colegio/lengua/tecnicas-grupales-expo/`;
     window.open(whatsappUrl, '_blank');
   });
   
